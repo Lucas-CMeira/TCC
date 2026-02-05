c89d89d0fe6327ef4a1371f4d2851519a9f5600c
Lucas-CMeira <lucascmeira2005@gmail.com>
2026-02-05 20:32:01 -0300

    FormsPt1

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ function App() {
 
   return (
     <>
-      <h1 className="text-red-600"> Acesse sua conta agora!</h1>
+    <div className="flex flex-col items-center justify-center mt-10 gap-4">
+      <h1>ORGANIZE SUAS FINANÇAS DE UMA FORMA MUITO MAIS PRÁTICA!</h1>
+    </div>
+    <div className="flex flex-col justify-center items-center gap-3">
+      <h2 className="text-red-600"> Acesse sua conta agora!</h2>
         <Form />
-      
+      </div>
     </>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,21 @@
 const forms = () => {
   return (
-    <div>
-      <input type="text" placeholder="Digite seu nome" id="name" />
+    
+  <div className= "w-96 h-96 border border-gray-400 rounded-lg shadow-md p-6 flex flex-col gap-4">
+    <div className="flex flex-col gap-1">
       <label htmlFor="name">Nome:</label>
+      <input type="text" placeholder="Digite seu nome" id="name" />
     </div>
+    <div className="flex flex-col gap-1">
+      <label htmlFor="email">Email:</label>
+      <input type="email" placeholder="Digite seu email" id="email" />
+    </div>
+    <div>
+      <a href="#" className="text-blue-500 hover:underline text-sm">Esqueceu sua senha?</a>
+    </div>
+    <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors">Entrar</button>
+  </div>
+    
   )
 }
 
